aa4888c177b04fda39b8d082dc51f28061fc6b6d
Jessi Sassy <asaprocky123@likelion.org>
2019-06-20 05:23:57 +0900

    settings.py corrected

--- project/settings.py
+++ project/settings.py
@@ -134,4 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # heroku
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
