790027e0ac149a64f3fe047d74f253fe24b27cfe
Jessi Sassy <asaprocky123@likelion.org>
2019-06-23 08:01:30 +0900

    heroku

--- project/settings.py
+++ project/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': 'eunjungleecub',
+        'USER': 'Eunjung Lee',
+        'PASSWORD': 'Ekakstp1996!'
+        'HOST': 'localhost',
+        'PORT': '',
+        }
 }
 
 
